39f0d3f5ae2fa7ceb923e3b3d994cb0229607280
Rudik Roberto Rompich <47486447+RudiksChess@users.noreply.github.com>
2020-02-08 16:01:37 -0600

    Update verificador.py

--- verificador.py
+++ verificador.py
@@ -1,1 +1,37 @@
-print("jeje")
+"""
+Created on Wed Feb  5 08:29:58 2020
+
+@author: Rudik Roberto Rompich
+@class: Herramientas Tecnologicas
+"""
+
+lol = input("\n|BIENVENIDO AL VERFICADOR DE CODIGOS: | \n" +
+
+            "SELECCIONE UNA OPCION: \n\n" +
+            "1. VERIFICAR \n" +
+            "2. DIGITO DE CONTROL\n" +
+            "3. SALIR \n\n" +
+            "OPCION:")
+
+while lol != "3":
+
+    if lol == "1":
+
+        print("jeje")
+
+
+    elif lol == "2":
+
+        print("xd")
+
+    else:
+
+        print("Vuelva a ingresar un valor valido")
+
+    lol = input("\n|BIENVENIDO AL VERFICADOR DE CODIGOS: | \n" +
+
+                "SELECCIONE UNA OPCION: \n\n" +
+                "1. VERIFICAR \n" +
+                "2. DIGITO DE CONTROL\n" +
+                "3. SALIR \n\n" +
+                "OPCION:")
