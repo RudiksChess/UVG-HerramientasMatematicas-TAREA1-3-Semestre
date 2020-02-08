fd158b130856e0a15d9e338809eb88bc0412e390
RudiksChess <rudikrrc@gmail.com>
2020-02-08 17:35:22 -0600

    Primer aporte, solo prueba

--- verificador.py
+++ verificador.py
@@ -5,33 +5,101 @@ Created on Wed Feb  5 08:29:58 2020
 @class: Herramientas Tecnologicas
 """
 
-lol = input("\n|BIENVENIDO AL VERFICADOR DE CODIGOS: | \n" +
+lol = int(input("\n|BIENVENIDO AL VERFICADOR DE CODIGOS: | \n" +
 
             "SELECCIONE UNA OPCION: \n\n" +
             "1. VERIFICAR \n" +
             "2. DIGITO DE CONTROL\n" +
             "3. SALIR \n\n" +
-            "OPCION:")
+            "OPCION:"))
 
-while lol != "3":
+while lol !=3:
 
-    if lol == "1":
+ try:
 
-        print("jeje")
+    if lol ==1:
+
+        x = int(input("¿Qué código quiere combrobar?: \n" +
+                          "1. UPC \n" +
+                          "2. ISBN - 10 \n" +
+                          "3. ISBN - 13 \n" +
+                          "4. NIT \n" +
+                          "5. Salir\n" +
+                          " Opción:"))
+        while x != 5:
+
+             if x ==1:
+                 print("1")
+             elif x ==2:
+                 print("2")
+             elif x ==3:
+                 print("3")
+             elif x ==4:
+                 print("4")
+             else:
+
+                 print("Ingresar valor válido")
+
+             x = int(input("¿Qué código quiere comprobar?: \n" +
+                           "1. UPC \n" +
+                           "2. ISBN - 10 \n" +
+                           "3. ISBN - 13 \n" +
+                           "4. NIT \n" +
+                           "5. Salir\n" +
+                           " Opción:"))
 
 
-    elif lol == "2":
 
-        print("xd")
+    elif lol ==2:
+
+      y = int(input("¿A 1ué código quiere generarle un número de verificación?: \n" +
+                    "1. UPC \n" +
+                    "2. ISBN - 10 \n" +
+                    "3. ISBN - 13 \n" +
+                    "4. NIT \n" +
+                    "5. Salir\n" +
+                    " Opción:"))
+      while y != 5:
+
+          if y ==1:
+              print("1")
+          elif y ==2:
+              print("2")
+          elif y ==3:
+              print("3")
+          elif y ==4:
+              print("4")
+          else:
+
+              print("Ingresar valor válido")
+
+          y = int(input("¿A 1ué código quiere generarle un número de verificación?: \n" +
+                        "1. UPC \n" +
+                        "2. ISBN - 10 \n" +
+                        "3. ISBN - 13 \n" +
+                        "4. NIT \n" +
+                        "5. Salir\n" +
+                        " Opción:"))
+
+
+
+
 
     else:
 
-        print("Vuelva a ingresar un valor valido")
+        print("Vuelva a ingresar un valor válido")
 
-    lol = input("\n|BIENVENIDO AL VERFICADOR DE CODIGOS: | \n" +
+
+
+
+    lol = int(input("\n|BIENVENIDO AL VERFICADOR DE CODIGOS: | \n" +
 
                 "SELECCIONE UNA OPCION: \n\n" +
                 "1. VERIFICAR \n" +
                 "2. DIGITO DE CONTROL\n" +
                 "3. SALIR \n\n" +
-                "OPCION:")
+                "OPCION:"))
+
+ except ValueError:
+
+     print("ERROR: Está ingresando un valor no númerico")
